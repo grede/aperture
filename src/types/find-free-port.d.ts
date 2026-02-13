578848d8c4f1c810e4cddfb9c740a1b7a77594c8
Kirill Rozhenkov <gredex0r@gmail.com>
2026-02-13 22:41:39 +0100

    Follow another idea

--- src/types/find-free-port.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'find-free-port' {
-  function findFreePort(startPort: number, endPort?: number, host?: string): Promise<[number]>;
-  export = findFreePort;
-}
--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Central export point for utility modules
- */
-
-export * from './logger.js';
-export * from './exec.js';
-export * from './retry.js';
-export * from './hash.js';
